a9bbe019f10ebd02865be586b608f22098dfac54
Nam Dinh <dinhtrongnam@gmail.com>
2019-01-13 03:58:39 +0700

    initial

---
+++ src/Requests/Backend/StorePermissionsRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Djavue\Auth\Requests\Backend;
+
+use Djavue\Auth\Models\Permission;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePermissionsRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return Permission::storeValidation($this);
+    }
+}
